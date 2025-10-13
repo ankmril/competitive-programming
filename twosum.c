b07d6d14cdd9450732fd2fa3c8bb1ca0f1840693
ivibewithghosts <ankmril9@gmail.com>
2025-10-13 19:37:01 +0530

    finished code with one issue - duplicate outputs

--- twosum.c
+++ twosum.c
@@ -7,5 +7,26 @@ int main() {
     // We can assume that each input will have one solution and it's never going to fail or be incorrect.
     // The answer can be returned in any order. It doesn't have to be chronological.
 
+    int n,i,j,target;
+
+    printf("Enter the size of array:\n");
+    scanf("%d",&n);
+
+    printf("Enter the elements of array:\n");
+    int nums[n];
+    for(i=0;i<n;i++)
+        scanf("%d",&nums[i]);
+    
+    printf("Enter the target value:\n");
+    scanf("%d",&target);
+
+    for(i=0;i<n;i++) {
+        for(j=0;j<n;j++) {
+            if(i!=j) {
+                if((nums[i]+nums[j])==target)
+                    printf("Positions of the elements: %d, %d\n",i,j);
+            }
+        }
+    }
     return 0;
 }
