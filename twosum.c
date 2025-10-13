17a1cf8a3d2048d21770efdae63adbc25529b6ad
ivibewithghosts <ankmril9@gmail.com>
2025-10-13 19:24:17 +0530

    initial lines of code

---
+++ twosum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    // We have been given an array 'nums' and an integer value 'target' in the question.
+    // We are supposed to print the positions of the elements of the array which add up to 'target'.
+    // We can assume that each input will have one solution and it's never going to fail or be incorrect.
+    // The answer can be returned in any order. It doesn't have to be chronological.
+
+    return 0;
+}
