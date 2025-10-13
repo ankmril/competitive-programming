856dc38bdebe1344a569ad37f71216412eccea6f
ivibewithghosts <ankmril9@gmail.com>
2025-10-13 19:44:57 +0530

    fixed the entire code

--- twosum.c
+++ twosum.c
@@ -1,30 +1,19 @@
 #include <stdio.h>
-
-int main() {
-
-    // We have been given an array 'nums' and an integer value 'target' in the question.
-    // We are supposed to print the positions of the elements of the array which add up to 'target'.
-    // We can assume that each input will have one solution and it's never going to fail or be incorrect.
-    // The answer can be returned in any order. It doesn't have to be chronological.
-
-    int n,i,j,target;
-
-    printf("Enter the size of array:\n");
-    scanf("%d",&n);
-
-    printf("Enter the elements of array:\n");
-    int nums[n];
-    for(i=0;i<n;i++)
-        scanf("%d",&nums[i]);
-    
-    printf("Enter the target value:\n");
-    scanf("%d",&target);
-
-    for(i=0;i<n;i++) {
-        for(j=0;j<n;j++) {
-            if((i!=j)&&((nums[i]+nums[j])==target))
-                    printf("Positions of the elements: %d, %d\n",i,j);
+#define NULL 0
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = 0; j < numsSize; j++) {
+            if (i != j && (nums[i] + nums[j] == target)) {
+                int *result = (int*)malloc(2 * sizeof(int));
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
         }
     }
-    return 0;
+    return NULL; // In case no match is found (won't happen per problem statement)
 }
